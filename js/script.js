be37eeefe85eadfb00ecb8ce753ddfc2a9aaa970
Maiorova Yuna <maiorova.yuna@gmail.com>
2022-10-16 17:42:06 +0300

    feat :add header and night/day mode

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const modeButton = document.querySelector('.lalalal');
+console.log(modeButton)
+let currentMode = true;
+
+modeButton.addEventListener('click', (e) => changeMode(e))
+
+function changeMode(e) {
+	const logo = document.querySelector('.header__logo');
+	const dayCat = logo.children[0];
+	const nightCat = logo.children[1];
+	const modable = Array.from(document.querySelectorAll('[data-mode]'));
+	if (currentMode) {
+		modable.forEach(i => i.dataset.mode = 'night');
+		nightCat.style.opacity = 0;
+		dayCat.style.opacity = 1;
+		e.currentTarget.textContent = 'DAY MODE'
+		currentMode = false;
+	} else {
+		modable.forEach(i => i.dataset.mode = 'day');
+		nightCat.style.opacity = 1;
+		dayCat.style.opacity = 0;
+		e.currentTarget.textContent = 'NIGHT MODE'
+		currentMode = true;
+	}
+}
